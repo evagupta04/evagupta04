e2ca9131dd4c66a8d131bc9796ac2fdacd07ee1a
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-04 01:08:33 +0530

    Time: 13 ms (61.57%), Space: 57.3 MB (27.11%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -6,9 +6,12 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
         //run a loop for each string of String Array
         for(String s : strs) {
-            //convert string to char Array and Sort it
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
+            //create a char array of size 26
+            char[] ch = new char[26];
+            //iterate over the char array(String converted), and increase each character's value by 1
+            for(char c : s.toCharArray()) {
+                ch[c - 'a']++;
+            }
             //convert char array, backto String
             String temp = String.valueOf(ch);
             //if map doesnt contains that string as key, then put it with a new ArrayList object
@@ -20,3 +23,25 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+//Using Sorting
+// public List<List<String>> groupAnagrams(String[] strs) {
+//         if(strs.length == 0 || strs == null)
+//             return new ArrayList<>();
+//         //create a map
+//         Map<String, List<String>> map = new HashMap<>();
+//         //run a loop for each string of String Array
+//         for(String s : strs) {
+//             //convert string to char Array and Sort it
+//             char[] ch = s.toCharArray();
+//             Arrays.sort(ch);
+//             //convert char array, backto String
+//             String temp = String.valueOf(ch);
+//             //if map doesnt contains that string as key, then put it with a new ArrayList object
+//             if(!map.containsKey(temp))
+//                 map.put(temp, new ArrayList<>());
+//             //otherwise put value into the existing key
+//             map.get(temp).add(s);
+//         }
+//         return new ArrayList<>(map.values());
+//     }
