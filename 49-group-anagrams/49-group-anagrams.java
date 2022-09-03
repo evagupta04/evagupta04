ad42e33b10c3f6f8097c31e7ee559e18300a3471
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-04 01:12:42 +0530

    Time: 11 ms (78.69%), Space: 56.9 MB (32.12%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -9,16 +9,19 @@ class Solution {
         for(String s : strs) {
             //create a char array of size 26
             char[] ch = new char[26];
+            
             //iterate over the char array(String converted), and increase each character's value by 1
             for(char c : s.toCharArray()) {
                 ch[c - 'a']++;
             }
+            
             //convert char array, back to String
             String temp = String.valueOf(ch);
             
             //if map doesnt contains that string as key, then put it with a new ArrayList object
             if(!map.containsKey(temp))
                 map.put(temp, new ArrayList<>());
+            
             //otherwise put value into the existing key
             map.get(temp).add(s);
         }
