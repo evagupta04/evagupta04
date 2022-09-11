7350200ff54ec79ed53c69368552b7fa2fcc21c8
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-11 16:23:04 +0530

    Time: 3 ms (38.06%), Space: 40.7 MB (82.56%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,8 +1,29 @@
 class Solution {
+    //binary search
+    //TC: log(root of x), much better than linear search, log bcz of binary search
+    //SC: o(1)
     public int mySqrt(int x) {
-        long y =0;
-        while(y*y <= x)
-            y++;
-        return (int)y-1;
+        long start =0, end = Integer.MAX_VALUE, ans = 0;
+        while(start <= end) {
+            long mid = start+(end-start)/2;
+            if(mid*mid <= x) {
+                ans = mid;
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+        }
+        return (int)ans;
     }
 }
+
+//linear search
+//TC: O(root of x)
+//SC: O(1)
+// public int mySqrt(int x) {
+//         long y =0;
+//         while(y*y <= x)
+//             y++;
+//         return (int)y-1;
+//     }
