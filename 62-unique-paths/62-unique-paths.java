10ce26bd08337f2a6cdd238aff79a361698417e4
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-11 12:55:04 +0530

    Time: 1 ms (53.51%), Space: 41.7 MB (7.40%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return countPaths(0, 0, m, n, dp);
+    }
+    
+    public int countPaths(int i, int j, int m, int n, int[][] dp) {
+        if(i == m-1 || j == n-1)
+            return 1;
+        if(i >= m || j >= n)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
+    }
+}
+
+//******** Recursive Approach, 
+//********TC: exponential bcz we are gonna try all possible combinations , SC: as we are using stack space, so exponential
+//  public int uniquePaths(int m, int n) {
+//         return countPaths(0, 0, m, n);
+//     }
+    
+//     public int countPaths(int i, int j, int m, int n) {
+//         if(i == m-1 || j == n-1)
+//             return 1;
+//         if(i >= m || j >= n)
+//             return 0;
+//         return countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n);
+//     }
