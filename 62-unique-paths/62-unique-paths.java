8269f3ab99dbff822a570be6d85a81cf1ee5b5a3
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-11 13:04:20 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (99.28%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,20 +1,17 @@
 class Solution {
+    //combinatorics approach, striver's approach
+    //The time complexity of this solution will be O(n-1) or  O(m-1) depending on the formula we are using.
+    //As we are not using any extra space the space complexity of the solution will be  O(1).
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+        int N = m + n-2;  //no of steps each time, m=3, n=7, n=7+3-2=8
+        int r = m-1;    //r=3-1=2
+        //int r = n-1;  //any one from m or n would do, we are taking m (m-1)
+        double res = 1;
+        
+        for(int i=1; i<=r; i++) {
+            res = res*(N-r+i)/i;    //nCr   7*(8/2)=28
         }
-        return countPaths(0, 0, m, n, dp);
-    }
-    
-    public int countPaths(int i, int j, int m, int n, int[][] dp) {
-        if(i == m-1 || j == n-1)
-            return 1;
-        if(i >= m || j >= n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
+        return (int)res;
     }
 }
 
@@ -30,4 +27,26 @@ class Solution {
 //         if(i >= m || j >= n)
 //             return 0;
 //         return countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n);
+//     }
+
+//-----------------------------------------------------------------------------------------------------
+//******dp approach
+//     //******The time complexity of this solution will be O(n*m) because at max there can be n*m number of states.
+//     //******As we are using extra space for the dummy matrix the space complexity will also be O(n*m).
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+//         return countPaths(0, 0, m, n, dp);
+//     }
+    
+//     public int countPaths(int i, int j, int m, int n, int[][] dp) {
+//         if(i == m-1 || j == n-1)
+//             return 1;
+//         if(i >= m || j >= n)
+//             return 0;
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+//         return dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
 //     }
