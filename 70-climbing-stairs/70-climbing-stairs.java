d0b8cf9b596151cdfa8f8d23d0af2bf12892024a
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-12 18:56:27 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (42.00%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,16 +1,17 @@
 class Solution {
-    //DP, Bottom Up approach
+    //DP + Optimization
     public int climbStairs(int n) {
         if(n <= 1)
             return 1;
         
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev1 = 1;
+        int prev2 = 2;
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int cur_val = prev1 + prev2;
+            prev1 = prev2;  
+            prev2 = cur_val; 
         }
-        return dp[n];
+        return prev2; //can't return cur_val, will fail, if input is 2, bcz cur_val will be initializsed to 0 and will return 0
     }
     
     //DP approach
@@ -39,6 +40,8 @@ class Solution {
     //     else return climbStairs(n-1) + climbStairs(n-2);
     // }
 
+
+
 //**********Recustion + Memorization (Top Down Approach)
 //Complexity : Time: O(n) ; Space: O(n)
     // public int climbStairs(int n) {
@@ -53,3 +56,19 @@ class Solution {
     //     map.put(n, climbSt(n-1, map) + climbSt(n-2, map));
     //     return map.get(n);
     // }
+
+
+//DP, Bottom Up approach
+//     public int climbStairs(int n) {
+//         if(n <= 1)
+//             return 1;
+        
+//         int[] dp = new int[n+1];
+//         dp[1] = 1;
+//         dp[2] = 2;
+//         for(int i=3; i<=n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+    
