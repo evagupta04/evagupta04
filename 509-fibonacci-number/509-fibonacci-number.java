b087b7452b47877d0fb60b01e8459e23ac6905f4
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-12 13:49:04 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (59.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,26 @@
 class Solution {
+    //memoization Top down
     public int fib(int n) {
-        //recursion
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fib(n,dp);
+    }
+    
+    public int fib(int n, int[] dp) {
         if(n <= 1)
             return n;
-        else 
-            return fib(n-1) + fib(n-2);
+        if(dp[n] != -1)
+            return dp[n];
+        else
+            return dp[n] = fib(n-1, dp) + fib(n-2, dp);
     }
 }
+
+
+//recursion
+// public int fib(int n) {
+//     if(n <= 1)
+//         return n;
+//     else 
+//         return fib(n-1) + fib(n-2);
+// }
