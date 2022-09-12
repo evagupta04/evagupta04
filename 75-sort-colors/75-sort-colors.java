23e60ddd6f873c2ac5484e98a9e24d6b3292e441
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-12 19:46:07 +0530

    Time: 1 ms (43.25%), Space: 42.3 MB (66.01%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,19 +1,38 @@
 class Solution {
     public void sortColors(int[] nums) {
-    //2-pass
-    int count0 = 0, count1 = 0, count2 = 0;
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] == 0) {count0++;}
-        if (nums[i] == 1) {count1++;}
-        if (nums[i] == 2) {count2++;}
-    }
-    for(int i = 0; i < nums.length; i++) {
-        if (i < count0) {
-            nums[i] = 0;}
-        else if (i <  count0 + count1) {    //count0 + count1 bcz i will be increatented upto count0(int if condition) and will not start from 0, for count2(else if)
-            nums[i] = 1;}
-        else {
-            nums[i] = 2;}
-    }
+    //1-pass solution
+    int p1 = 0, p2 = nums.length-1;
+        for(int i=0; i<=p2; i++) {
+            if(nums[i] == 0) {
+                nums[i] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if(nums[i]  == 2) {
+                nums[i] = nums[p2];
+                nums[p2] = 2 ;
+                p2--;
+                i--;
+            }
+        }
     }
 }
+
+//2-pass solution
+// public void sortColors(int[] nums) {
+//         int count0 = 0, count1 = 0, count2 = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 0) {count0++;}
+//             if (nums[i] == 1) {count1++;}
+//             if (nums[i] == 2) {count2++;}
+//         }
+//         for(int i = 0; i < nums.length; i++) {
+//             if (i < count0) 
+//                 nums[i] = 0;
+//             //count0 + count1 bcz i will be increatented upto count0(int if condition) and will not start from 0, for count2(else if)
+//             else if (i <  count0 + count1) {    
+//                 nums[i] = 1;}
+//             else 
+//                 nums[i] = 2;
+//         }
+//     }
