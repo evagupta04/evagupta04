4c8043e4f2d9ba4daef7e33157a4bcfabf4b94b9
evagupta04 <111646381+evagupta04@users.noreply.github.com>
2022-09-12 17:57:18 +0530

    Time: 1 ms (9.97%), Space: 41.1 MB (27.41%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,41 @@
+class Solution {
+    //Recustion + Memorization (Top Down Approach)
+    //Complexity : Time: O(n) ; Space: O(n)
+    public int climbStairs(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1,1);   //for one steop, there will be 1 way
+        map.put(2,2);   //for two steps, there will be 2 ways
+        return climbSt(n, map);
+    }
+    public int climbSt(int n, Map<Integer, Integer> map) {
+        if(map.containsKey(n))
+            return map.get(n);
+        map.put(n, climbSt(n-1, map) + climbSt(n-2, map));
+        return map.get(n);
+    }
+    
+    //DP approach
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 2;
+        
+//         if(n <= 2)
+//             return dp[n];
+//         for(int i=3; i<=n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+}
+
+//********recursiveway, giving TLE
+//Complexity : Time: O(2^n) ; Space: O(n)
+    // public int climbStairs(int n) {
+    //     if(n == 1)
+    //         return 1;
+    //     if(n == 2)
+    //         return 2;
+    //     else return climbStairs(n-1) + climbStairs(n-2);
+    // }
